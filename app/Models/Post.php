3ee2039a62eb18f31eb6bc5fb3ceee84b13393c4
Aditya-fmh <adityafirmanshah09@gmail.com>
2024-06-21 16:04:49 +0700

    Implement Eloquent

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,40 +2,9 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => '1',
-                'slug' => 'waduh',
-                'title' => 'Waduh',
-                'author' => 'Aditya',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit et amet blanditiis, ipsum consequatur, quis aperiam totam vel iure delectus dolore ab quas officiis quod dicta rerum iste eaque?',
-                'published_at' => '2024-06-21',
-            ],
-            [
-                'id' => '2',
-                'slug' => 'waduh-2',
-                'title' => 'Waduh 2',
-                'author' => 'Aditya',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit et amet blanditiis, ipsum consequatur, quis aperiam totam vel iure delectus dolore ab quas officiis quod dicta rerum iste eaque?',
-                'published_at' => '2024-06-21',
-            ]
-        ];
-    }
-
-    public static function find($slug): array
-    {
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-
-        if (!$post) {
-            abort(404, 'Post not found');
-        }
-
-        return $post;
-    }
 }
