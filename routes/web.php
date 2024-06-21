9d8969e3f842fe2c4bdcae1e7fac550e101c7394
Aditya-fmh <adityafirmanshah09@gmail.com>
2024-06-21 14:50:18 +0700

    Another fixing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -12,51 +13,13 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => [
-        [
-            'id' => '1',
-            'title' => 'Waduh',
-            'author' => 'Aditya',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit et amet blanditiis, ipsum consequatur, quis aperiam totam vel iure delectus dolore ab quas officiis quod dicta rerum iste eaque?',
-            'published_at' => '2024-06-21',
-        ],
-        [
-            'id' => '2',
-            'title' => 'Waduh 2',
-            'author' => 'Aditya',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit et amet blanditiis, ipsum consequatur, quis aperiam totam vel iure delectus dolore ab quas officiis quod dicta rerum iste eaque?',
-            'published_at' => '2024-06-21',
-        ]
-    ]]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
 
-Route::get('/posts/{id}', function ($id) {
-    $posts = [
-        [
-            'id' => 1,
-            'title' => 'Waduh',
-            'author' => 'Aditya',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit et amet blanditiis, ipsum consequatur, quis aperiam totam vel iure delectus dolore ab quas officiis quod dicta rerum iste eaque?',
-            'published_at' => '2024-05-21',
-        ],
-        [
-            'id' => 2,
-            'title' => 'Waduh 2',
-            'author' => 'Aditya',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit et amet blanditiis, ipsum consequatur, quis aperiam totam vel iure delectus dolore ab quas officiis quod dicta rerum iste eaque?',
-            'published_at' => '2024-06-21',
-        ]
-    ];
-
-    $post = Arr::first($posts, function ($post) use ($id) {
-        return $post['id'] == $id;
-    });
-
+Route::get('/posts/{slug}', function ($slug) {
+    $post = Post::find($slug);
     return view('post', ['title' => 'Single Post', 'post' => $post]);
-    
 });
-
-
 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
