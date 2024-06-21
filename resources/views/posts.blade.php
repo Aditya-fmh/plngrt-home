ad1fe61c6df0396bdbf13ebb5fb0929c5669a904
Aditya F. <adityafirmanshah09@gmail.com>
2024-06-21 09:45:57 +0700

    Added single post view

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,14 +2,18 @@
   <x-slot:title>{{ $title }}</x-slot:title>
 
   @foreach ($posts as $post)
+
   <article class="py-8 max-w-screen-md border-b border-gray-300">
-    <h2 class="mb-4 text-3xl tracking-tight font-bold text-gray-900">Waduh</h2>
-    <div class="text-base text-gray-500">
-      <a href="#">Aditya</a> | 21 June 2024
+    <a href="/posts/{{ $post['id'] }}" >
+      <h2 class="mb-4 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
+    </a>
+      <div class="text-base text-gray-500">
+      <a href="#">{{ $post['author'] }}</a> | 21 June 2024
     </div>
-    <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit et amet blanditiis, ipsum consequatur, quis aperiam totam vel iure delectus dolore ab quas officiis quod dicta rerum iste eaque?</p>
-    <a href="#" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
+    <p class="my-4 font-light">{{ Str::limit($post['body'], 100) }}</p>
+    <a href="/posts/{{ $post['id'] }}" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
   </article>
+
   @endforeach
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,12 +14,14 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => [
         [
+            'id' => '1',
             'title' => 'Waduh',
             'author' => 'Aditya',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit et amet blanditiis, ipsum consequatur, quis aperiam totam vel iure delectus dolore ab quas officiis quod dicta rerum iste eaque?',
             'published_at' => '2024-06-21',
         ],
         [
+            'id' => '2',
             'title' => 'Waduh 2',
             'author' => 'Aditya',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit et amet blanditiis, ipsum consequatur, quis aperiam totam vel iure delectus dolore ab quas officiis quod dicta rerum iste eaque?',
@@ -26,6 +29,34 @@ Route::get('/posts', function () {
         ]
     ]]);
 });
+
+Route::get('/posts/{id}', function ($id) {
+    $posts = [
+        [
+            'id' => 1,
+            'title' => 'Waduh',
+            'author' => 'Aditya',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit et amet blanditiis, ipsum consequatur, quis aperiam totam vel iure delectus dolore ab quas officiis quod dicta rerum iste eaque?',
+            'published_at' => '2024-05-21',
+        ],
+        [
+            'id' => 2,
+            'title' => 'Waduh 2',
+            'author' => 'Aditya',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit et amet blanditiis, ipsum consequatur, quis aperiam totam vel iure delectus dolore ab quas officiis quod dicta rerum iste eaque?',
+            'published_at' => '2024-06-21',
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($id) {
+        return $post['id'] == $id;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+    
+});
+
+
 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
